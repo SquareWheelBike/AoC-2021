740a9a259d0c938f15f5cc440e8cc02944f2d9c5
SquareWheelBike <colefuerth@gmail.com>
2021-12-03 14:39:40 -0500

    made a template

---
+++ template/AoC.py
@@ -0,0 +1,17 @@
+import sys
+
+# AoC template for python3
+
+def main():
+    # start by getting file as a list of strings
+    f = [l.strip() for l in open(sys.argv[1], 'r')]
+
+    # PART 1
+    print("Part 1:") # part 1 answer
+
+    # PART 2
+    print("Part 2:") # part 2 answer
+
+# I need to get into the habit of using the main function
+if __name__ == "__main__":
+    main()

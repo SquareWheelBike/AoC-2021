6a2be92b582670fcfca73daacdba6d7c55a2493f
SquareWheelBike <colefuerth@gmail.com>
2021-12-07 09:12:00 -0500

    practice day 8

---
+++ 2020 practice/day08/AoC.py
@@ -0,0 +1,11 @@
+import sys
+
+def main():
+    # start by getting file as a list of strings
+    f = [l.strip() for l in open(sys.argv[1], 'r')]
+
+
+
+# I need to get into the habit of using the main function
+if __name__ == "__main__":
+    main()
